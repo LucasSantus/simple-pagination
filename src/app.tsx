1742cad1572d7a0f7a8694d66828c56784b139a6
Lucas Santos <leos.developer@gmail.com>
2024-02-19 22:48:48 -0300

    :rocket: feat: create skeleton on page

--- src/app.tsx
+++ src/app.tsx
@@ -1,60 +1,68 @@
-import { Plus, Search, Filter, FileDown, MoreHorizontal } from 'lucide-react'
-import { Header } from './components/header'
-import { Tabs } from './components/tabs'
-import { Button } from './components/ui/button'
-import { Control, Input } from './components/ui/input'
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
-import { Pagination } from './components/pagination'
-import { useQuery, keepPreviousData } from '@tanstack/react-query'
-import { useSearchParams } from 'react-router-dom'
-import { useState } from 'react'
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { FileDown, Filter, MoreHorizontal, Plus, Search } from "lucide-react";
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { Header } from "./components/header";
+import { Pagination } from "./components/pagination";
+import { Tabs } from "./components/tabs";
+import { Button } from "./components/ui/button";
+import { Control, Input } from "./components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./components/ui/table";
 
 export interface TagResponse {
-  first: number
-  prev: number | null
-  next: number
-  last: number
-  pages: number
-  items: number
-  data: Tag[]
+  first: number;
+  prev: number | null;
+  next: number;
+  last: number;
+  pages: number;
+  items: number;
+  data: Tag[];
 }
 
 export interface Tag {
-  title: string
-  amountOfVideos: number
-  id: string
+  title: string;
+  amountOfVideos: number;
+  id: string;
 }
 
 export function App() {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const urlFilter = searchParams.get('filter') ?? ''
-  
-  const [filter, setFilter] = useState(urlFilter)
+  const [searchParams, setSearchParams] = useSearchParams();
+  const urlFilter = searchParams.get("filter") ?? "";
 
-  const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1
+  const [filter, setFilter] = useState(urlFilter);
+
+  const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
+  const rowsPerPage = searchParams.get("rows-per-page") ?? "10";
 
   const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
-    queryKey: ['get-tags', urlFilter, page],
+    queryKey: ["get-tags", urlFilter, page, rowsPerPage],
     queryFn: async () => {
-      const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=10&title=${urlFilter}`)
-      const data = await response.json()
+      const response = await fetch(
+        `http://localhost:3333/tags?_page=${page}&_per_page=${rowsPerPage}&title=${urlFilter}`
+      );
+      const data = await response.json();
 
-      return data
+      await new Promise((res) => setTimeout(res, 5000));
+
+      return data;
     },
     placeholderData: keepPreviousData,
-  })
+  });
 
   function handleFilter() {
-    setSearchParams(params => {
-      params.set('page', '1')
-      params.set('filter', filter)
+    setSearchParams((params) => {
+      params.set("page", "1");
+      params.set("filter", filter);
 
-      return params
-    })
-  }
-
-  if (isLoading) {
-    return null
+      return params;
+    });
   }
 
   return (
@@ -63,22 +71,22 @@ export function App() {
         <Header />
         <Tabs />
       </div>
-      <main className="max-w-6xl mx-auto space-y-5">
+      <main className="max-w-6xl mx-auto space-y-5 transition-all delay-300 duration-300">
         <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <Button variant='primary'>
+          <Button variant="primary">
             <Plus className="size-3" />
             Create new
           </Button>
         </div>
 
         <div className="flex items-center justify-between">
-          <div className="flex items-center">
-            <Input variant='filter'>
+          <div className="flex items-center gap-3">
+            <Input variant="filter">
               <Search className="size-3" />
-              <Control 
-                placeholder="Search tags..." 
-                onChange={e => setFilter(e.target.value)}
+              <Control
+                placeholder="Search tags..."
+                onChange={(e) => setFilter(e.target.value)}
                 value={filter}
               />
             </Input>
@@ -94,42 +102,74 @@ export function App() {
           </Button>
         </div>
 
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead></TableHead>
-              <TableHead>Tag</TableHead>
-              <TableHead>Amount of videos</TableHead>
-              <TableHead></TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {tagsResponse?.data.map((tag) => {
-              return (
-                <TableRow key={tag.id}>
-                  <TableCell></TableCell>
-                  <TableCell>
-                    <div className="flex flex-col gap-0.5">
-                      <span className="font-medium">{tag.title}</span>
-                      <span className="text-xs text-zinc-500">{tag.id}</span>
-                    </div>
-                  </TableCell>
-                  <TableCell className="text-zinc-300">
-                    {tag.amountOfVideos} video(s)
-                  </TableCell>
-                  <TableCell className="text-right">
-                    <Button size="icon">
-                      <MoreHorizontal className="size-4" />
-                    </Button>
-                  </TableCell>
+        {isLoading ? (
+          <Table>
+            <TableHeader>
+              <TableRow>
+                {Array.from({ length: 4 }).map((_, index) => (
+                  <TableHead key={index}>
+                    <div className="w-full h-5 bg-zinc-900 animate-pulse rounded-md" />
+                  </TableHead>
+                ))}
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Array.from({ length: 10 }).map((_, index) => (
+                <TableRow key={index}>
+                  {Array.from({ length: 4 }).map((_, cellIndex) => (
+                    <TableCell key={cellIndex}>
+                      <div className="w-full h-9 bg-zinc-900 animate-pulse rounded-md" />
+                    </TableCell>
+                  ))}
                 </TableRow>
-              )
-            })}
-          </TableBody>
-        </Table>
+              ))}
+            </TableBody>
+          </Table>
+        ) : (
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead></TableHead>
+                <TableHead>Tag</TableHead>
+                <TableHead>Amount of videos</TableHead>
+                <TableHead></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {tagsResponse?.data.map((tag) => {
+                return (
+                  <TableRow key={tag.id}>
+                    <TableCell></TableCell>
+                    <TableCell>
+                      <div className="flex flex-col gap-0.5">
+                        <span className="font-medium">{tag.title}</span>
+                        <span className="text-xs text-zinc-500">{tag.id}</span>
+                      </div>
+                    </TableCell>
+                    <TableCell className="text-zinc-300">
+                      {tag.amountOfVideos} video(s)
+                    </TableCell>
+                    <TableCell className="text-right">
+                      <Button size="icon">
+                        <MoreHorizontal className="size-4" />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        )}
 
-        {tagsResponse && <Pagination pages={tagsResponse.pages} items={tagsResponse.items} page={page} />}
+        {tagsResponse && (
+          <Pagination
+            pages={tagsResponse.pages}
+            items={tagsResponse.items}
+            page={page}
+            showingItems={tagsResponse.data.length}
+          />
+        )}
       </main>
     </div>
-  )
+  );
 }
